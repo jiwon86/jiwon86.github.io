29d67c79be54b785b039f41a18bbf85534f12dd1
jiwon86 <94026114+jiwon86@users.noreply.github.com>
2023-06-15 22:35:10 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+let viewer = document.querySelector(".imgViewer");
+let img = document.querySelectorAll(".picture");
+let closeButt = document.querySelector(".closeButt");
+
+
+function imgPopOn (e) {
+    console.log(e.target.getBoundingClientRect().x);
+    viewer.classList.toggle("on");
+}
+
+function imgPopOff() {
+    viewer.classList.toggle("on");
+    viewer.classList.toggle("off");
+}
+
+function imgPopReset(e){
+    if(e.animationName == "imgOff") {
+        viewer.classList.toggle("off");
+    }
+}
+
+// Event Set
+
+img.forEach(element => {
+    element.addEventListener("click", imgPopOn);
+});
+
+closeButt.addEventListener("click", imgPopOff);
+viewer.addEventListener("animationend" , imgPopReset)
