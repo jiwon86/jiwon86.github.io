1ce2bd1afa064a1e1a39962aaf8c6f3b74d15e62
jiwon86 <94026114+jiwon86@users.noreply.github.com>
2023-06-20 00:04:03 +0900

    2023 0620
    2023 0620

--- main.js
+++ main.js
@@ -1,24 +1,74 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-let viewer = document.querySelector(".imgViewer");
+let viewer = document.querySelector(".Mygalery");
+let main = document.querySelector(".main");
 let img = document.querySelectorAll(".picture");
 let closeButt = document.querySelector(".closeButt");
 
 
+// SWiper Main Set
+
+let swiper = new Swiper('.main', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: false,
+    mousewheel:true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    }
+ });
+
+ // SWiper Galery Set
+
+ let swiper2 = new Swiper('.Mygalery', {
+    // Optional parameters
+    loop: true,
+    loopAdditionalSlides : 1,
+    centeredSlides: true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//----------------------------------------------
+
 function imgPopOn (e) {
-    console.log(e.target.getBoundingClientRect().x);
+    var index = e.target.alt;
     viewer.classList.toggle("on");
+    
+    swiper2.slideToLoop(index, 1 , false);
+    console.log(index);  
+    // swiper Disable
+    swiper.disable();
 }
 
 function imgPopOff() {
     viewer.classList.toggle("on");
     viewer.classList.toggle("off");
+    
+    // swiper Enable
+    swiper.enable();
 }
 
 function imgPopReset(e){
     if(e.animationName == "imgOff") {
         viewer.classList.toggle("off");
+        
     }
 }
 
@@ -29,4 +79,4 @@ img.forEach(element => {
 });
 
 closeButt.addEventListener("click", imgPopOff);
-viewer.addEventListener("animationend" , imgPopReset)
+viewer.addEventListener("animationend" , imgPopReset);
