9a77b25734daca20a684b975c45e7ff0a39a30b8
jiwon86 <94026114+jiwon86@users.noreply.github.com>
2023-06-26 22:19:57 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 let viewer = document.querySelector(".Mygalery");
 let main = document.querySelector(".main");
 let img = document.querySelectorAll(".picture");
 let closeButt = document.querySelector(".closeButt");
-
-// resize Event
-window.addEventListener('resize', () => {
-    vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-window.addEventListener('touchend', () => {
-    vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+let introText = document.querySelector(".text1");
+let inText = new Array(4);
+let event1 = 0;
 
 
 // SWiper Main Set
@@ -24,7 +15,6 @@ let swiper = new Swiper('.main', {
     direction: 'vertical',
     loop: false,
     mousewheel:true,
-    autoHeight:true,
     spaceBetween:0,
     // If we need pagination
     pagination: {
@@ -35,7 +25,10 @@ let swiper = new Swiper('.main', {
     // And if we need scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
-    }
+    },
+    on : {
+        slideChangeTransitionEnd : aa
+    },
  });
 
  // SWiper Galery Set
@@ -56,6 +49,32 @@ let swiper = new Swiper('.main', {
         prevEl: ".swiper-button-prev",
     },
 });
+
+function aa(e) {
+    var index = swiper.activeIndex;
+    if(index == 1 && event1 == 0){
+        for (let i = 0; i < inText.length; i++) {
+            var id = "t" + i
+            inText[i] = document.getElementById(id);
+            inText[i].addEventListener("animationend" , function(){
+                textAnime(i);
+            });
+            
+        }
+        event1 = 1;
+        inText[0].classList.toggle("tt");
+    }
+}
+
+function textAnime(i){
+    console.log(inText[i])
+    if (i != 3){
+        inText[i+1].classList.toggle("tt");
+    }else{
+        inText[i].classList.toggle("flower1");
+      //document.getElementById("tab2").classList.toggle("flower1");
+    }
+}
 
 //----------------------------------------------
 
@@ -87,11 +106,43 @@ function imgPopReset(e){
     }
 }
 
+// Map Set
+var LatLng = [37.3082126, 126.828793]
+var weedingHole = new naver.maps.LatLng(LatLng[0], LatLng[1]);
+
+var mapOptions = {
+    center: weedingHole,
+    zoom: 17,
+    draggable: false,
+    pinchZoom: false,
+    scrollWheel: false,
+    keyboardShortcuts: false,
+    disableDoubleTapZoom: true,
+    disableDoubleClickZoom: true,
+    disableTwoFingerTapZoom: true
+};
+
+var map = new naver.maps.Map('map', mapOptions);
+
+var markerOptions = {
+    position: weedingHole,
+    map: map,
+    icon : {
+        url : "./img/marker.png"
+        ,scaledSize : new naver.maps.Size(45,45)
+        ,animation: naver.maps.Animation.BOUNCE
+    }
+};
+
+var marker = new naver.maps.Marker(markerOptions);
+marker.setAnimation(naver.maps.Animation.BOUNCE);
+
 // Event Set
 
 img.forEach(element => {
     element.addEventListener("click", imgPopOn);
 });
+
 
 closeButt.addEventListener("click", imgPopOff);
 viewer.addEventListener("animationend" , imgPopReset);
