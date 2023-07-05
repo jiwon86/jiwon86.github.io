c90115c5198466e175f03fda3735696d94bba573
jiwon86 <94026114+jiwon86@users.noreply.github.com>
2023-07-05 21:08:41 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -51,11 +51,9 @@ let swiper = new Swiper('.main', {
 //----------------------------------------------
 
 function imgPopOn (e) {
-    var index = e.target.alt;
+    var index = e.target.getAttribute("index");
     viewer.classList.toggle("on");
 
-    vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
     swiper2.init();
     swiper2.slideToLoop(index, 1 , false);
     console.log(index);  
@@ -82,6 +80,18 @@ function imgPopReset(e){
 var LatLng = [37.3082126, 126.828793]
 var weedingHole = new naver.maps.LatLng(LatLng[0], LatLng[1]);
 
+var park = [
+    new naver.maps.LatLng(37.3088757, 126.8272830) // 양지주차타워
+    ,new naver.maps.LatLng(37.3074087, 126.8279742) // 대림프라자앞공영주차장공영주차장
+    ,new naver.maps.LatLng(37.3089053, 126.8277812) // mk주차타워
+];
+
+var parkInfo = [
+    "https://naver.me/5MCOh9Am"
+    ,"https://naver.me/5vIcSZne"
+    ,"https://naver.me/5eGTN7fW"
+];
+
 var mapOptions = {
     center: weedingHole,
     zoom: 17,
@@ -105,12 +115,57 @@ var markerOptions = {
         ,animation: naver.maps.Animation.BOUNCE
     }
 };
+for (let i = 0; i < 3; i++) {
+
+    var markerOptions2 = {
+        position: park[i],
+        map: map,
+        icon : {
+            url : "./img/marker/p"+ (i+2) +".png"
+            ,scaledSize : new naver.maps.Size(30,40)
+        }
+    };
+    var parkmarker = new naver.maps.Marker(markerOptions2);
+
+    naver.maps.Event.addListener(parkmarker, "click" , function(){
+        window.open(parkInfo[i], "_blank");
+    });
+    
+}
+
 
 var marker = new naver.maps.Marker(markerOptions);
 marker.setAnimation(naver.maps.Animation.BOUNCE);
+
 naver.maps.Event.addListener(marker, "click" , function(){
     window.open("https://naver.me/IxD7uOQW", "_blank");
 });
+
+
+
+
+function moveMap(x){
+    console.log("asd")
+    switch (x) {
+        case 1:
+            map.setCenter(weedingHole);
+            break;
+        case 2:
+            map.setCenter(park[x-2]);
+                break;
+        case 3:
+            map.setCenter(park[x-2]);
+                break;
+        case 4:
+            map.setCenter(park[x-2]);
+                break;
+    
+        default:
+            break;
+    }
+   
+}
+
 // Event Set
 
 img.forEach(element => {
