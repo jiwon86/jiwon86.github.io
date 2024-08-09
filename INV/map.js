8b70f873392f2eb75dfbb4d438c41420972bcf37
jiwon86 <94026114+jiwon86@users.noreply.github.com>
2024-08-10 00:01:53 +0900

    Add files via upload

---
+++ INV/map.js
@@ -0,0 +1,34 @@
+// Map Set
+var LatLng = [34.8490397, 126.492639]
+var inv = new naver.maps.LatLng(LatLng[0], LatLng[1]);
+
+var mapOptions = {
+    center: inv,
+    zoom: 18,
+    draggable: true,
+    pinchZoom: true,
+    scrollWheel: true,
+    keyboardShortcuts: false,
+    disableDoubleTapZoom: true,
+    disableDoubleClickZoom: true,
+    disableTwoFingerTapZoom: true
+};
+
+var map = new naver.maps.Map('map', mapOptions);
+
+var markerOptions = {
+    position: inv,
+    map: map,
+};
+
+var marker = new naver.maps.Marker(markerOptions);
+//marker.setAnimation(naver.maps.Animation.BOUNCE);
+
+naver.maps.Event.addListener(marker, "click" , function(){
+    window.open("https://naver.me/xJNpT7SU", "_blank");
+});
+
+
+naver.maps.Event.addListener(map, "click" , function(e){
+    console.log(e);
+});
